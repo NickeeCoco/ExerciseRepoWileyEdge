ab522ea1e1473ee20c33af8ff9ed3b1bad5fe6a0
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-01 14:48:32 -0500

    stay positive exercise

--- PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/whiles/StayPositive.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/whiles/StayPositive.java
@@ -1,15 +1,38 @@
 package com.sg.foundations.flowcontrol.whiles;
 
+import java.util.Scanner;
+
 public class StayPositive {
     public static void main(String[] args) {
-        int start = 10;
+        Scanner userInput = new Scanner(System.in);
+        int start = 0;
+        boolean isValid = false;
 
-        System.out.println("Counting down...");
+        do {
+           try {
+               if(start < 0) {
+                   System.out.println("The integer can not be a negative integer.");
+               }
+               System.out.print("What number should I count down from? ");
+               start = Integer.parseInt(userInput.nextLine());
+               isValid = true;
+           } catch(NumberFormatException ex) {
+               System.out.println("Please input a valid integer.");
+           }
+        } while(!isValid || start < 0);
+
+        System.out.println("");
+        System.out.println("Here goes!");
         System.out.println("");
 
         while(start >= 0) {
-            System.out.println(start);
-            start--;
+            for(int i = 0; i < 10; i++) {
+                if(start >= 0) {
+                    System.out.print(start + " ");
+                    start--;
+                }
+            }
+            System.out.println("");
         }
 
         System.out.println("");
