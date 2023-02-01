3a63b8249f723cfb1f45b31f3578f8ea2fea73ce
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-01 11:15:44 -0500

    error handling for user's input

--- WindowMaster/src/main/java/com/wiley_edge/windowmaster/WindowMaster.java
+++ WindowMaster/src/main/java/com/wiley_edge/windowmaster/WindowMaster.java
@@ -2,62 +2,103 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.wiley_edge.windowmaster;
 
 import java.util.Scanner;
 
 /**
- *
  * @author Nickee Coco
  */
 public class WindowMaster {
 
     public static void main(String[] args) {
-        float height;
-        float width;
-        float glassCost;
-        float trimCost;
-        int nbWindows;
-        
+        float height = 0;
+        float width = 0;
+        float glassCost = 0;
+        float trimCost = 0;
+        int nbWindows = 0;
+
         String strHeight;
         String strWidth;
-        String strGlass;
-        String strTrim;
+        String strGlassCost;
+        String strTrimCost;
         String strNbWindows;
-        
+
         float area;
         float perimeter;
         float totalCost;
-        
+
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Please enter the window's height:");
-        strHeight = sc.nextLine();
-        
-        System.out.println("Please enter the window's width:");
-        strWidth = sc.nextLine();
-        
-        System.out.println("Please enter the glass's cost:");
-        strGlass = sc.nextLine();
-        
-        System.out.println("Please enter the trim's cost:");
-        strTrim = sc.nextLine();
-        
-        System.out.println("Please enter the number of windows:");
-        strNbWindows = sc.nextLine();
-        
-        height = Float.parseFloat(strHeight);
-        width = Float.parseFloat(strWidth);
-        glassCost = Float.parseFloat(strGlass);
-        trimCost = Float.parseFloat(strTrim);
-        nbWindows = Integer.parseInt(strNbWindows);
-        
+
+        boolean isValid;
+
+        do {
+            isValid = false;
+            try {
+                System.out.println("Please enter the window's height:");
+                strHeight = sc.nextLine();
+                height = Float.parseFloat(strHeight);
+                isValid = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Input could not be parsed into a float");
+            }
+        } while (!isValid);
+
+        do {
+            isValid = false;
+            try {
+                System.out.println("Please enter the window's width:");
+                strWidth = sc.nextLine();
+                width = Float.parseFloat(strWidth);
+                isValid = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Input could not be parsed into a float");
+            }
+        } while (!isValid);
+
+        do {
+            isValid = false;
+            try {
+                System.out.println("Please enter the glass's cost:");
+                strGlassCost = sc.nextLine();
+                glassCost = Float.parseFloat(strGlassCost);
+                isValid = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Input could not be parsed into a float");
+            }
+        } while (!isValid);
+
+        do {
+            isValid = false;
+            try {
+                System.out.println("Please enter the trim's cost:");
+                strTrimCost = sc.nextLine();
+                trimCost = Float.parseFloat(strTrimCost);
+                isValid = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Input could not be parsed into a float");
+
+            }
+        } while (!isValid);
+
+        do {
+            isValid = false;
+            try {
+                System.out.println("Please enter the number of windows:");
+                strNbWindows = sc.nextLine();
+                nbWindows = Integer.parseInt(strNbWindows);
+                isValid = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Input could not be parsed into an integer");
+
+            }
+        } while (!isValid);
+
         area = height * width;
         perimeter = 2 * (height + width);
-        
+
         totalCost = nbWindows * ((glassCost * area) + (trimCost * perimeter));
-        
+
         System.out.println("Window's height: " + height);
         System.out.println("Window's width: " + width);
         System.out.println("Window's area: " + area);
