c78dd7cf6918305e6422608f467c511b484111b3
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-07 12:07:58 -0500

    refactor healthy hearts

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/healthyhearts/App.java
@@ -0,0 +1,8 @@
+package com.sg.foundations.practice_exercises.healthyhearts;
+
+public class App {
+    public static void main(String[] args) {
+        HealthyHearts healthyHearts = new HealthyHearts();
+        healthyHearts.analyzeHeartHealth();
+    }
+}
--- PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/healthyhearts/HealthyHearts.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/healthyhearts/HealthyHearts.java
@@ -2,54 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.sg.foundations.practice_exercises;
+package com.sg.foundations.practice_exercises.healthyhearts;
 
 import java.util.Scanner;
 
 /**
- *
  * @author Nickee Coco
  */
 public class HealthyHearts {
-    public static void main(String[] args) {
-        
-        int age = 0;
-        int maximumHeartRate;
-        
-        // values for calculation
-        double minRangeD;
-        double maxRangeD;
-        
-        // values for display
-        int minRangeInt;
-        int maxRangeInt;
-        
-        boolean isValid = false;
-        
-        Scanner userInput = new Scanner(System.in);
-        
+    int age = 0;
+    int maximumHeartRate;
+
+    // values for calculation
+    double minRangeD;
+    double maxRangeD;
+
+    // values for display
+    int minRangeInt;
+    int maxRangeInt;
+
+    boolean isValid = false;
+
+    Scanner userInput = new Scanner(System.in);
+
+    public void analyzeHeartHealth() {
+
         do {
             try {
-                System.out.println("What is your age?");
+                System.out.print("What is your age? ");
                 int input = Integer.parseInt(userInput.nextLine());
-                if(input <= 0 || input > 130){
+                if (input <= 0 || input > 130) {
                     System.out.println("Please input an integer between 1 and 130");
                 } else {
                     age = input;
                     isValid = true;
                 }
-            } catch(NumberFormatException ex) {
+            } catch (NumberFormatException ex) {
                 System.out.println("Please input an integer between 1 and 130");
             }
-        } while(!isValid);
-        
+        } while (!isValid);
+
         maximumHeartRate = 220 - age;
         minRangeD = maximumHeartRate / 2;
-        maxRangeD = 85.0 / 100.0 * maximumHeartRate; 
-        
+        maxRangeD = 85.0 / 100.0 * maximumHeartRate;
+
         minRangeInt = (int) Math.ceil(minRangeD);
         maxRangeInt = (int) Math.ceil(maxRangeD);
-        
+
         System.out.println("Your maximum heart rate should be " + maximumHeartRate + " beats per minute.");
         System.out.println("Your target HR Zone is " + minRangeInt + " - " + maxRangeInt + " beats per minute.");
     }
