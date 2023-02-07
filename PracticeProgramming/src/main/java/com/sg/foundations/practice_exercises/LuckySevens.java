45c5563852899d53a8d4edb4add2e5e591c558aa
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-07 11:15:59 -0500

    lucky sevens exercise

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/LuckySevens.java
@@ -0,0 +1,59 @@
+package com.sg.foundations.practice_exercises;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class LuckySevens {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        int totalAmount = 0;
+        int rolls = 0;
+        int maxAmount = 0;
+        int maxAmountRoll = 0;
+
+        while(totalAmount <= 0) {
+            try {
+                System.out.print("How many dollars do you have? ");
+                totalAmount = Integer.parseInt(userInput.nextLine());
+            } catch(NumberFormatException ex) {
+                System.out.println("Please input an integer!");
+            }
+        }
+
+        // set max amount to initial amount
+        maxAmount = totalAmount;
+
+        while(totalAmount > 0) {
+            rolls++;
+
+            if(rollDice()) {
+                totalAmount += 4;
+                if(totalAmount > maxAmount) {
+                    maxAmount = totalAmount;
+                    maxAmountRoll = rolls;
+                }
+            } else {
+                totalAmount -= 1;
+            }
+        }
+
+        System.out.println("You are broke after " + rolls + " rolls.");
+        System.out.println("You should have quit after " + maxAmountRoll + " rolls when you had $" + maxAmount + ".");
+    }
+
+    public static boolean rollDice() {
+        Random rdm = new Random();
+
+        int die1 = rdm.nextInt(6) + 1;
+        int die2 = rdm.nextInt(6) + 1;
+
+        int sum = die1 + die2;
+
+        if(sum != 7) {
+            return false;
+        }
+
+        return true;
+    }
+}
