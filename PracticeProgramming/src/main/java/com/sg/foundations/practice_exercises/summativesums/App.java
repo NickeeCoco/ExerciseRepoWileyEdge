be0fb7fc1c4a3b16083bacf93a40bee6d058531c
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-07 12:10:29 -0500

    refactor summative sums

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/summativesums/App.java
@@ -0,0 +1,8 @@
+package com.sg.foundations.practice_exercises.summativesums;
+
+public class App {
+    public static void main(String[] args) {
+        SummativeSums summativeSums = new SummativeSums();
+        summativeSums.addArrays();
+    }
+}
--- PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/summativesums/SummativeSums.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/summativesums/SummativeSums.java
@@ -2,30 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.sg.foundations.practice_exercises;
+package com.sg.foundations.practice_exercises.summativesums;
 
 /**
- *
  * @author Nickee Coco
  */
 public class SummativeSums {
-    public static void main(String[] args) {
-        int[] testArr1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
-        int[] testArr2 = { 999, -60, -77, 14, 160, 301 };
-        int[] testArr3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99 };
-        
+    int[] testArr1 = {1, 90, -33, -55, 67, -16, 28, -55, 15};
+    int[] testArr2 = {999, -60, -77, 14, 160, 301};
+    int[] testArr3 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99};
+
+    public void addArrays() {
+
         System.out.println("#1 Array Sum: " + addArray(testArr1));
         System.out.println("#2 Array Sum: " + addArray(testArr2));
         System.out.println("#3 Array Sum: " + addArray(testArr3));
     }
-    
+
     public static int addArray(int[] operands) {
         int total = 0;
-        
-        for(int operand : operands) {
+
+        for (int operand : operands) {
             total += operand;
         }
-        
+
         return total;
     }
 }
