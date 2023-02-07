9e2f6f2577588ff11677d71661e4f23afc81c903
Morgane Bentzinger <mo.bentzinger@gmail.com>
2023-02-07 12:05:35 -0500

    refactor dog genetics

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/doggenetics/App.java
@@ -0,0 +1,8 @@
+package com.sg.foundations.practice_exercises.doggenetics;
+
+public class App {
+    public static void main(String[] args) {
+        DogGenetics dogGenetics = new DogGenetics();
+        dogGenetics.analyseDog();
+    }
+}
--- PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/doggenetics/DogGenetics.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/practice_exercises/doggenetics/DogGenetics.java
@@ -2,36 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.sg.foundations.practice_exercises;
+package com.sg.foundations.practice_exercises.doggenetics;
 
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- *
  * @author Nickee Coco
  */
 public class DogGenetics {
 
-    public static void main(String[] args) {
-        final int NUMBER_OF_DOG_RACES = 5;
-        String dogName;
-        
-        Scanner userInput = new Scanner(System.in);
-        
+    final int NUMBER_OF_DOG_RACES = 5;
+    String dogName;
+
+    Scanner userInput = new Scanner(System.in);
+
+    public void analyseDog() {
+
         System.out.print("What is your dog's name? ");
-        dogName = userInput.nextLine();      
-        
+        dogName = userInput.nextLine();
+
         int currentBound = 100;
 
         int[] percentages = new int[NUMBER_OF_DOG_RACES];
-        
+
         // the last value not be generated randomly
         // it will be the remainder value to reach 100 total
-        for (int i = 0; i < percentages.length - 1; i++) { 
+        for (int i = 0; i < percentages.length - 1; i++) {
             Random rd = new Random();
-            
-            if(currentBound > 0) {
+
+            if (currentBound > 0) {
                 int rdValue = rd.nextInt(currentBound + 1); // add one to include the value of currentBound
                 percentages[i] = rdValue;
                 currentBound -= rdValue;
@@ -39,22 +39,23 @@ public class DogGenetics {
                 break;
             }
         }
-        
+
         percentages[NUMBER_OF_DOG_RACES - 1] = currentBound;
-        
+
         System.out.println("Well then, I have this highly reliable report on " + dogName + "'s prestigious background right here.");
         System.out.println();
-        
+
         System.out.println(dogName + " is:");
         System.out.println();
-        
+
         System.out.println(percentages[0] + "% St. Bernard");
         System.out.println(percentages[1] + "% Chihuahua");
         System.out.println(percentages[2] + "% Dramatic RedNosed Asian Pug");
         System.out.println(percentages[3] + "% Common Cur");
         System.out.println(percentages[4] + "% King Doberman");
-        
+
         System.out.println();
         System.out.println("Wow, that's QUITE the dog!");
     }
+
 }
